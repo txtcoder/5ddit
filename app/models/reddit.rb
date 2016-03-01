30f378a86c9a73742e7a694c778bd87db32ee913
txtcoder <tommytong.is.here+txtcoder@gmail.com>
2016-03-01 22:04:30 +0000

    ban SandersForPresident subreddit for having so many editorialized title although I really support Bernie Sanders

--- app/models/reddit.rb
+++ app/models/reddit.rb
@@ -25,7 +25,7 @@ include HTTParty
         end
         banned_url =["imgur", "facebook", "youtu","meme","wikipedia","self","gfycat","twitter", "docs.google.com", "streamable","reddit"]
         banned_extension=[".gif",".png",".jpg",".pdf", ".gifv"]
-        banned_subreddit=["funny","aww","earthporn","gifs","pics","mildlyinteresting","todayilearned","h3h3productions"]
+        banned_subreddit=["funny","aww","earthporn","gifs","pics","mildlyinteresting","todayilearned","h3h3productions","SandersForPresident"]
         top5title=[]
         lowestscore=9999
         after=""
