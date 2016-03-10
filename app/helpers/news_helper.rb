f2bb2f8a6471a67d026cc53a9453915d13a2659e
txtcoder <tommytong.is.here+txtcoder@gmail.com>
2016-03-10 03:04:59 +0000

    comment UI slightly prettier

--- app/helpers/news_helper.rb
+++ app/helpers/news_helper.rb
@@ -1,18 +1,20 @@
 module NewsHelper
-    def showComments(json)
+    def showComments(json, i=0)
        res=""
         return "" if json.nil?
        json.each do |x|
-            res << "<li>"
+            res << "<li class='#{if i%2==0 then "even" else "odd" end}'>"
+            res << "<div class ='commentTitle'>"
             res << "Posted by: " + "<span>" + x[:author] << "</span>"
             res << " Score:" + "<span>" + x[:score].to_s
             res << "</span>"
-            res << "<div>"
+            res << "</div>"
+            res << "<div class ='commentContent'>"
             res <<  x[:comment]
             res << "</div>"
             if x[:replies]!=nil
                 res << "<ul>"
-                res << showComments(x[:replies])
+                res << showComments(x[:replies], i+1)
                 res << "</ul>"
             end
             res << "</li>"
