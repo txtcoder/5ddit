57342996a66654fc8a24c60c83d12188f46fd745
txtcoder <tommytong.is.here+txtcoder@gmail.com>
2016-03-25 19:35:57 +0000

    ban woahdude

--- app/models/reddit.rb
+++ app/models/reddit.rb
@@ -25,7 +25,7 @@ include HTTParty
         end
         banned_url =["imgur", "facebook", "youtu","meme","wikipedia","self","gfycat","twitter", "docs.google.com", "streamable","reddit","vimeo","liveleak","imgflip"]
         banned_extension=[".gif",".png",".jpg",".pdf", ".gifv",".mp3",".mp4",".mov"]
-        banned_subreddit=["funny","aww","earthporn","gifs","pics","mildlyinteresting","todayilearned","h3h3productions","videos","wtf","adviceanimals"]
+        banned_subreddit=["funny","aww","earthporn","gifs","pics","mildlyinteresting","todayilearned","h3h3productions","videos","wtf","adviceanimals","woahdude"]
         us_limit_subreddit=["news","politics","sandersforpresident"]
         entertainment_limit_subreddit=["music","movies","books","television"]
         top5title=[]
