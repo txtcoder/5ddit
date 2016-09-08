fc3ea75abbeafe632b3dbad452b7de337921b62a
txtcoder <tommytong.is.here+txtcoder@gmail.com>
2016-09-08 05:13:32 +0000

    test: show user_agent

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,6 +1,7 @@
 class NewsController < ApplicationController
   def index
     user_agent = request.env['HTTP_USER_AGENT']
+    @user_agent = user_agent
     if user_agent.index('X-Requested-With')
         @from_app = true
     else
