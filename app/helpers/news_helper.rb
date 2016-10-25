f9b91b1f372fc75f98de9bfd1956d7ff501aa736
txtcoder <tommytong.is.here+txtcoder@gmail.com>
2016-10-25 04:22:38 +0000

    x

--- app/helpers/news_helper.rb
+++ app/helpers/news_helper.rb
@@ -5,16 +5,16 @@ module NewsHelper
        json.each do |x|
             res << "<li class='#{if i%2==0 then "even" else "odd" end}'>"
             res << "<div class ='commentTitle'>"
-            res << "Posted by: " + "<span>" + x[:author] << "</span>"
-            res << " Score:" + "<span>" + x[:score].to_s
+            res << "Posted by: " + "<span>" + x["author"] << "</span>"
+            res << " Score:" + "<span>" + x["score"].to_s
             res << "</span>"
             res << "</div>"
             res << "<div class ='commentContent'>"
-            res <<  x[:comment]
+            res <<  x["comment"]
             res << "</div>"
-            if x[:replies]!=nil
+            if x["replies"]!=nil
                 res << "<ul>"
-                res << showComments(x[:replies], i+1)
+                res << showComments(x["replies"], i+1)
                 res << "</ul>"
             end
             res << "</li>"
