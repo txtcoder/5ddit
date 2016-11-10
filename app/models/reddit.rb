69eb173b56b3cb0b52cf80b8207d4c194a5a6af1
txtcoder <tommytong.is.here+txtcoder@gmail.com>
2016-11-10 17:03:15 +0000

    nerf US politics

--- app/models/reddit.rb
+++ app/models/reddit.rb
@@ -39,7 +39,7 @@ include HTTParty
         banned_url =["imgur", "facebook", "youtu","meme","wikipedia","gfycat","twitter", "docs.google.com", "streamable","reddituploads","vimeo","liveleak","imgflip","giphy","sli.mg","oddshot.tv","spotify","chzbgr","tumblr","battle.net","twitch.tv","instagram","plus.google","thepoke.co.uk","deviantart","twimg.com","imgfly","imgcert","i.redd.it","google.com"]
         banned_extension=[".gif",".png",".jpg",".pdf", ".gifv",".mp3",".mp4",".mov",".jpeg"]
         banned_subreddit=["funny","aww","earthporn","gifs","pics","mildlyinteresting","todayilearned","h3h3productions","videos","wtf","adviceanimals","woahdude","subredditsimulator","movies","music","books","television","comics","gaming","dota2","programming","xboxone","nottheonion","overwatch","pokemongo","globaloffensive","pcgaming","dataisbeautiful","starwars","makingamurderer","upliftingnews","leagueoflegends","hearthstone","showerthoughts","tifu","bestof","reddeadredemption","ps4","pokemon","destinythegame","explainlikeimfive","britishproblems","lifeprotips","jokes","askreddit","iama","internetisbeautiful","savedyouaclick"]
-        us_nerfed_subreddit=["news"]
+        us_nerfed_subreddit=["news","politics","the_donald"]
         top5title=[]
         lowestscore=9999
         after=""
@@ -59,7 +59,7 @@ include HTTParty
 
                 score=x["data"]["score"]-(time-x["data"]["created_utc"].to_i).to_i/36
                 if us_nerfed_subreddit.any? {|y| x["data"]["subreddit"].downcase==y}
-                    score=score*0.8
+                    score=score*0.7
                 end
                
                 origscore=x["data"]["score"]
