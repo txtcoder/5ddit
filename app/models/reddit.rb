810622297f922159217a9a396f6375e37ba95577
txtcoder <tommytong.is.here+txtcoder@gmail.com>
2017-03-13 03:33:30 +0000

    ban nsfw

--- app/models/reddit.rb
+++ app/models/reddit.rb
@@ -66,6 +66,7 @@ include HTTParty
                 next if banned_subreddit.any? {|y| x["data"]["subreddit"].downcase==y}
 
                 next if x["data"]["is_self"]
+                next if x["data"]["thumbnail"]=="nsfw"
 
                 score=x["data"]["score"]*(100-1.15*(((time-x["data"]["created_utc"].to_i).to_i/3600)**2)/10)/100.0
                 if us_nerfed_subreddit.any? {|y| x["data"]["subreddit"].downcase==y}
