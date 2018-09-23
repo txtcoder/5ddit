467c48735cbb6384dcd58ed3c0a2554e43b0d005
Tommy Tong <tommytong.is.here+txtcoder@gmail.com>
2018-09-23 10:11:29 -0400

    more us nerf

--- app/models/reddit.rb
+++ app/models/reddit.rb
@@ -52,7 +52,7 @@ include HTTParty
         banned_url =["imgur", "facebook", "youtu","meme","wikipedia","gfycat","twitter", "docs.google.com", "streamable","reddituploads","vimeo","liveleak","imgflip","giphy","sli.mg","oddshot.tv","spotify","chzbgr","tumblr","battle.net","twitch.tv","instagram","plus.google","thepoke.co.uk","deviantart","twimg.com","imgfly","imgcert","i.redd.it","google.com","screenpranks","change.org","redd.it","steam","images.duckduckgo","vid.me","reddit","itunes.apple.com","strawpoll.com","wizards.com","cc.com","flickr","netflix.com","apps.varden.info","theonion.com","clippituser.tv","streamja"]
         banned_extension=[".gif",".png",".jpg",".pdf", ".gifv",".mp3",".mp4",".mov",".jpeg"]
         banned_subreddit=["funny","aww","earthporn","gifs","pics","mildlyinteresting","todayilearned","h3h3productions","videos","wtf","adviceanimals","woahdude","subredditsimulator","dota2","programming","xboxone","overwatch","pokemongo","globaloffensive","pcgaming","dataisbeautiful","starwars","makingamurderer","leagueoflegends","hearthstone","showerthoughts","tifu","bestof","reddeadredemption","ps4","pokemon","destinythegame","explainlikeimfive","britishproblems","lifeprotips","jokes","askreddit","iama","internetisbeautiful","savedyouaclick","circlejerk","enoughtrumpspam","marchagainsttrump","esist","impeach_trump","osugame","me_irl","garlicoin"]
-        us_nerfed_subreddit=["news","politics","the_donald"]
+        us_nerfed_subreddit=["news","politics","the_donald", "the_mueller"]
         agenda_nerfed_subreddit=["trees","atheism","conspiracy","twoxchromosomes","lgbt"]
         entertainment_nerfed_subreddit=["movies","music","books","television","comics","gaming","upliftingnews","mma"]
         stupid_nerfed_subreddit=["nottheonion"]
